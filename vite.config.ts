61419308c28bf28a11be8bf988ab389cfbe50412
magedalhilali <maged.alhilali705@gmail.com>
2026-01-09 08:44:27 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // THIS LINE IS CRITICAL FOR GITHUB PAGES
-      base: '/RGCT_COUNTER/', 
+      // REQUIRED: Matches your repository name
+      base: '/RGCT_COUNTER/',
       server: {
         port: 3000,
         host: '0.0.0.0',
